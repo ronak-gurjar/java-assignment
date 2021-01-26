fc64ff7b17563e34cfa8c974007591fead4914f3
ronak-gurjar <ronakgurjar029@gmail.com>
2021-01-26 16:45:50 +0530

    test

--- com/uks/ronak/core/day8/EmpInfoMerger.java
+++ com/uks/ronak/core/day8/EmpInfoMerger.java
@@ -9,7 +9,7 @@ public class EmpInfoMerger extends Thread {
     String file;
     String mode;
     boolean flag = false;
-    // shared variable to store file data
+    // shared variable for store file data
     static List<String> list = new ArrayList<>();
 
     EmpInfoMerger(String file, String mode) {
@@ -18,7 +18,7 @@ public class EmpInfoMerger extends Thread {
     }
 
     public void writeToResource() throws IOException, InterruptedException {
-        FileReader fileRead = new FileReader(file);
+        File fileRead = new File(file);
         Scanner scanner = new Scanner(fileRead);
 
         while (scanner.hasNextLine()) {
@@ -32,21 +32,23 @@ public class EmpInfoMerger extends Thread {
     public void writeToFile() throws IOException, InterruptedException {
         File fileWrite = new File(file);
         if (fileWrite.exists()) {
-            FileWriter write = new FileWriter(fileWrite, true);
-            if (list == null) {
-                System.out.println("shared variable is null");
-            } else {
-                for (int i = 0; i < list.size(); i++) {
-                    synchronized (this) {
-                        String str = list.get(i);
-                        write.write(str + "\n");
-                        System.out.println(str);
+            if (list != null) {
+                FileWriter write = new FileWriter(fileWrite, true);
+                if (list == null) {
+                    System.out.println("shared variable is null");
+                } else {
+                    for (int i = 0; i < list.size(); i++) {
+                        synchronized (this) {
+                            String str = list.get(i);
+                            write.write(str + "\n");
+                            System.out.println(str);
+                        }
                     }
                 }
+                list.clear();
+                sleep(2000);
+                write.close();
             }
-            list.clear();
-            sleep(2000);
-            write.close();
         } else {
             fileWrite.createNewFile();
             writeToFile();
@@ -54,6 +56,7 @@ public class EmpInfoMerger extends Thread {
     }
 
     public void run() {
+
         if (mode.equalsIgnoreCase("read")) {
             try {
                 writeToResource();
@@ -62,6 +65,7 @@ public class EmpInfoMerger extends Thread {
             }
         } else {
             try {
+                sleep(2000);
                 writeToFile();
             } catch (IOException | InterruptedException ioException) {
                 ioException.printStackTrace();
@@ -76,10 +80,12 @@ public class EmpInfoMerger extends Thread {
     }
 
     public static void main(String[] args) throws InterruptedException, IOException {
-        EmpInfoMerger nameFileRead = new EmpInfoMerger("src\\com\\uks\\ronak\\core\\day8\\name.txt", "read");
-        EmpInfoMerger addFileRead = new EmpInfoMerger("src\\com\\uks\\ronak\\core\\day8\\address.txt", "read");
-        EmpInfoMerger newFileWrite = new EmpInfoMerger("src\\com\\uks\\ronak\\core\\day8\\newFileName.txt", "write");
+        EmpInfoMerger nameFileRead = new EmpInfoMerger("/home/reo/IdeaProjects/brilworks assignment/src/com/uks/ronak/core/day8/name.txt", "read");
+        EmpInfoMerger addFileRead = new EmpInfoMerger("/home/reo/IdeaProjects/brilworks assignment/src/com/uks/ronak/core/day8/address.txt", "read");
+        EmpInfoMerger newFileWrite = new EmpInfoMerger("/home/reo/IdeaProjects/brilworks assignment/src/com/uks/ronak/core/day8/newFileName.txt", "write");
         nameFileRead.start();
+        // addFile thread sleep for 20ms
+        sleep(20);
         addFileRead.start();
         sleep(2000);
         newFileWrite.start();
