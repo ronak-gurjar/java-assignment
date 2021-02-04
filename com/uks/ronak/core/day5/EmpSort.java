3550b1d6056a9bcf395bd2b716bb97951657594c
ronak-gurjar <ronakgurjar029@gmail.com>
2021-02-04 10:53:06 +0530

    making new class

---
+++ com/uks/ronak/core/day5/EmpSort.java
@@ -0,0 +1,81 @@
+package com.uks.ronak.core.day5;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmpSort {
+
+    // constant values
+    public static final String eCODE = "ecode";
+    public static final String fName = "fname";
+    public static final String lName = "lname";
+    public static final String eType = "etype";
+    public static final String salary = "salary";
+
+    public List<EmployeeSortedList> sortedList(String cName) throws FileNotFoundException {
+        File file = new File("src/com/uks/ronak/core/day5/employeeData.txt");
+        Scanner scanner = new Scanner(file);
+        String data;
+        while (scanner.hasNext()) {
+            data = scanner.nextLine();
+            String[] token = data.split(",");
+            EmployeeSortedList employee = new EmployeeSortedList(Integer.parseInt(token[0]), token[1], token[2], token[3], Long.parseLong(token[4]));
+            list.add(employee);
+        }
+        switch (cName) {
+            case eCODE:
+                Collections.sort(list);
+                //Collections.reverse(list);
+                break;
+            case fName:
+                // Collections.sort(list,new FName());
+                list.sort(new EmployeeSortedList.FName());
+                break;
+            case lName:
+                list.sort(new EmployeeSortedList.LName());
+                break;
+            case eType:
+                list.sort(new EmployeeSortedList.EType());
+                break;
+            case salary:
+                list.sort(new EmployeeSortedList.Salary());
+                break;
+            default:
+                System.out.println("wrong choice");
+                break;
+        }
+        return list;
+    }
+
+    List<EmployeeSortedList> list = new ArrayList<>();
+
+    public void print() {
+        for (EmployeeSortedList employeeSortedList : list) {
+            System.out.printf("%1s", employeeSortedList.getEmpCode());
+            System.out.printf("%10s", employeeSortedList.getFirstName() + " ");
+            System.out.printf("%5s", employeeSortedList.getLastName());
+            System.out.printf("%15s", employeeSortedList.getEmpType());
+            System.out.printf("%10s", employeeSortedList.getBasicSalary() + "\n");
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+        EmployeeSortedList employeeSortedList = new EmployeeSortedList(1, "dfd", "Ddd", "dd", 2);
+        EmpSort empSort = new EmpSort();
+        System.out.println("1 -> default sorting based on empcode ");
+        System.out.println("or choose [fname],[lname],[salary],[etype]");
+        String cName = scanner.nextLine();
+        if (cName.equals("1")) {
+            empSort.sortedList("ecode");
+            empSort.print();
+        } else {
+            empSort.sortedList(cName);
+            empSort.print();
+        }
+    }
+}
